28912cfd1a1db9d58bae5d26912c07dcd611291f
vacuous_cosmos <codingshiba2237@gmail.com>
2023-04-02 16:02:55 +0530

    Added two handler functions

--- main.go
+++ main.go
@@ -7,6 +7,12 @@ import (
 
 // Home Handler Function
 func home(w http.ResponseWriter, r *http.Request) {
+	//check if the currentl request URL path matf=ches "/"
+	// otherwise send a 404 response
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	w.Write([]byte("Hello from snippetbox"))
 }
 
@@ -17,6 +23,13 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 
 // snippetcreate Handler Function
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost { //check request type
+		//if not post then writeheader as 405 and send a method not allowed response
+		w.Header().Set("Allow", "POST")
+		//calls writeHeader and write function behind the scenes
+		http.Error(w, "Method Not Allowed", 405)
+		return
+	}
 	w.Write([]byte("Create a new request"))
 }
 func main() {
