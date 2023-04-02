dc52ded3510bdd3ba1cf1cd7feca05566bbaa0d7
vacuous_cosmos <codingshiba2237@gmail.com>
2023-04-02 15:22:03 +0530

    added a base handler function and router

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
+// Home Handler Function
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from snippetbox"))
+}
 func main() {
-	fmt.Println("Hello world!")
+	//http.NewServeMux() function to init a new servermux=>equivalent to a router
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	log.Print("Starting server on :4000")
+	// server creation
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 }
