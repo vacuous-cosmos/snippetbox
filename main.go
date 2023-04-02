eb137bf00db1306983562e8038d81242cfd39d56
vacuous_cosmos <codingshiba2237@gmail.com>
2023-04-02 15:34:15 +0530

    added snippetview and snippetcreate

--- main.go
+++ main.go
@@ -9,10 +9,22 @@ import (
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from snippetbox"))
 }
+
+// snippetView Handler Function
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("create a new snippet"))
+}
+
+// snippetcreate Handler Function
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new request"))
+}
 func main() {
 	//http.NewServeMux() function to init a new servermux=>equivalent to a router
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 	log.Print("Starting server on :4000")
 	// server creation
 	err := http.ListenAndServe(":4000", mux)
