cc6e3662927bfab3e598725f4b90512f74a3f475
vacuous_cosmos <codingshiba2237@gmail.com>
2023-04-03 23:49:23 +0530

    added css,js

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -8,6 +8,10 @@ import (
 func main() {
 	//http.NewServeMux() function to init a new servermux=>equivalent to a router
 	mux := http.NewServeMux()
+	//create a file server which serves files out of the "./ui/static" directory
+	fileServer := http.FileServer(http.Dir("./ui/static"))
+	//we have to strip static prefix
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
