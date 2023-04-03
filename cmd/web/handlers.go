8ef025c91b075009c75d39ee5a3ba90d63497976
vacuous_cosmos <codingshiba2237@gmail.com>
2023-04-03 23:13:12 +0530

    Added navigation template

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -16,7 +16,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
-	files := []string{"./ui/html/base.tmpl", "./ui/html/pages/home.tmpl"}
+	files := []string{"./ui/html/base.tmpl", "./ui/html/pages/home.tmpl", "./ui/html/partials/nav.tmpl"}
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
 		log.Print(err.Error())
