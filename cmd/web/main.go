f62b64cb882f734c30bf49f937efab353fd6aaf4
vacuous_cosmos <codingshiba2237@gmail.com>
2023-04-08 22:31:51 +0530

    added command line flag for port spec

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	//Defining a  new commandline flag with the name 'addr',a default value of ":4000"
+	//and some short help text explaining what the flag controls.The value of
+	//the flag will be stored in addr variable at runtime
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	//flag.Parse will read the command line value incase of no value default value will be provided
+	flag.Parse()
 	//http.NewServeMux() function to init a new servermux=>equivalent to a router
 	mux := http.NewServeMux()
 	//create a file server which serves files out of the "./ui/static" directory
@@ -15,8 +22,8 @@ func main() {
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
-	log.Print("Starting server on :4000")
+	log.Printf("Starting server on :%s", *addr)
 	// server creation
-	err := http.ListenAndServe(":4000", mux)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 }
