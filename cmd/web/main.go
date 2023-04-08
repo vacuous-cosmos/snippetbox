c95a5f875739fb38705a05583132104a8fd66bc1
vacuous_cosmos <codingshiba2237@gmail.com>
2023-04-08 22:55:15 +0530

    custom loggers added

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -13,6 +14,9 @@ func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	//flag.Parse will read the command line value incase of no value default value will be provided
 	flag.Parse()
+	//using log.New to create a new log with three params first is where to output second is prefix third is format
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime)
 	//http.NewServeMux() function to init a new servermux=>equivalent to a router
 	mux := http.NewServeMux()
 	//create a file server which serves files out of the "./ui/static" directory
@@ -22,8 +26,16 @@ func main() {
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
-	log.Printf("Starting server on :%s", *addr)
+	//Initialize a new http.Server struct.We Set the addr and handler fields so
+	//that the server uses the same network uses the same net addr and routes as before.
+	//we init the ErrorLog fieldso that server uses customErrorLogger field in case of any errors
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+	infoLog.Printf("Starting server on :%s", *addr)
 	// server creation
-	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+	err := srv.ListenAndServe(*addr, mux)
+	errorLog.Fatal(err)
 }
